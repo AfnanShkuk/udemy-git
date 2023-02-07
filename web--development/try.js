acc1ac02579d5645ad608cffcd55590dea4f53f4
Afnanshkuk <afnanadel91@gmail.com>
2023-02-07 12:13:03 +0300

    final assignment

---
+++ web--development/try.js
@@ -0,0 +1,54 @@
+var x= 10.1;
+var y="she said: \"I'm Afnan\"";
+var z= true;
+var f= false;
+document.write();
+var a=0, b=10;
+a = ++b
+document.write();
+var x=10;
+var y="10";
+document.write()
+var x=10;
+var y=9;
+var z=12;
+document.write()
+/* && true+true =true
+ && true+false ,false+true =false
+ && false+false=false */
+/* || true+true=true
+ || true+false+true
+  || false+false=false */
+/* !true=false
+!false = true */
+/* if (condition){ 
+  statement;
+} */
+var mynum1 = 7;
+var mynum2= 10;
+if (mynum1 < mynum2){
+  document.write("7 is less than 10")
+}
+var age=15
+if(age >=18){
+  document.write("yor are allowed");
+}
+else if(age>=15){
+  document.write("return later");
+}
+else {
+  document.write("you are not allowed");
+}
+ // alert("welcome to my website! \n welcome to my world!");
+ // var user= prompt("enter your name: ");
+//var result=confirm("Do you Agree?");
+//if(result==true){
+  //alert("Thank You!");
+  else {
+    //alert("Oh! No!");
+}
+function change_color(){
+  var dd1=
+  document.getelementById("d1");
+  var dd2=
+  document.getelementById("d2");
